542cd9514aa4f27766068a47ee9cefa80d193f49
Paul Gege <pgege@macbookpro.home>
2026-01-21 00:28:49 -0500

    fix: restore API health check endpoint

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
 import { MedusaModule } from './medusa/medusa.module';
 import { CustomerModule } from './customer/customer.module';
 import { EventsModule } from './events/events.module';
 
 @Module({
   imports: [MedusaModule, CustomerModule, EventsModule],
+  controllers: [AppController],
 })
 export class AppModule {}
